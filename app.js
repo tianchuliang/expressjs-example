405a533a3cf0212e6679d1bd3d32140b15a3a246
Tianchu Liang <liangtianchu@gmail.com>
2015-10-31 11:07:56 -0400

    Modified app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var app = express();
 app.use(bodyParser.json());
 
 // Include the Todo-lists service and let it bind to the application
-var todoService = require('./services/todo.js');
+var todoService = require('./services/user.js');
 todoService.LinkApp(app);
 
 var server = app.listen(8080, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('Direct your browser to http://%s:%s/api/todo-lists', host, port);
+	console.log('Direct your browser to http://%s:%s/api/users', host, port);
 });
